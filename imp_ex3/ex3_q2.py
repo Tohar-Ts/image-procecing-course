c1ab6d4d62574ffe69f04ee75a5680a6a4a9d29e
tohar <toharbk@gmail.com>
2021-12-11 23:33:54 +0200

    q2

--- imp_ex3/ex3_q2.py
+++ imp_ex3/ex3_q2.py
@@ -6,14 +6,13 @@ EX3 q2
 import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
+from numpy.core.fromnumeric import shape
 
 def create_masks(shape):
     
-    row, col  = int(shape/2), int(shape/2)
-    urow, drow = row+10, row-10
-    ucol, dcol = col+10, col-10
+    row, col = shape
+    row, col  = int(row/2), int(col/2)
 
-    masks = []
     mask1 = np.ones(shape, np.uint16)
     mask1[:,col-10:col+10] = 0
 
@@ -25,7 +24,7 @@ def create_masks(shape):
     mask4 = cv.bitwise_not(mask2)
    
     mask5 = np.zeros(shape, np.uint16)
-    mask5[row-10:row+10,col-10:col+10] = 255
+    mask5[row-10:row+10, col-10:col+10] = 255
     
     mask6 = cv.bitwise_not(mask5)
     
@@ -33,9 +32,42 @@ def create_masks(shape):
     mask7[row-10:row+10,:] = 0
     
     mask8 = cv.bitwise_not(mask7) 
-    
-    
+
+    return [mask1, mask2, mask3, mask4, mask5, mask6, mask7, mask8]
+
+
+def fft(img, masks):
+    fft_img = np.fft.fft2(img)
+    res = []
+
+    for i, m in enumerate(masks):
+        img_mask = np.multiply(fft_img, m)
+        shifted = np.fft.ifftshift(img_mask)
+        fft_final = np.fft.ifft2(shifted)
+        res.append(np.uint16(np.abs(fft_final)))
+        
+    return res
+
+
 if __name__ == '__main__':
-    img = plt.imread("fimage_.png")
-    plt.imshow(img)
+    img = cv.imread("fimage_.png", cv.IMREAD_GRAYSCALE)
+    print(img.shape)
+    
+    f, x = plt.subplots(4, 2)
+    masks = create_masks(img.shape)
+    m = 0
+    for r in range(2):
+        for c in range(4):
+            x[c,r].imshow(masks[m], cmap = "gray")
+            m+=1
+    plt.show()
+
+    fft_images = fft(img, masks)
+
+    f, x = plt.subplots(4, 2)
+    f_img = 0
+    for r in range(2):
+        for c in range(4):
+            x[c,r].imshow(fft_images[f_img], cmap = "gray")
+            f_img+=1
     plt.show()
